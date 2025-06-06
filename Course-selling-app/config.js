dcc6b9e1678622c7cf52b1280f5071bd3ad3b747
Neel Khatri <neelkhatri7780@gmail.com>
2025-06-06 18:06:06 +0530

    Whole Course-Selling-app updated

--- Course-selling-app/config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config(); 
-
-const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
-const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
-
-module.exports = {
-    JWT_USER_PASSWORD,
-    JWT_ADMIN_PASSWORD,
-}
