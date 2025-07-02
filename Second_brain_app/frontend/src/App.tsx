184588559c3b7c588f511fe8a8fe73e94dae61b3
Neel Khatri <neelkhatri7780@gmail.com>
2025-07-02 11:33:30 +0530

    Added Card components

--- Second_brain_app/frontend/src/App.tsx
+++ Second_brain_app/frontend/src/App.tsx
@@ -1,13 +1,18 @@
 
-import {Button} from "./components/ui/button"
-import {PlusIcon} from "./assets/icons"
+// import {Button} from "./components/ui/button"
+import {Card} from "./components/ui/cards"
+// import {PlusIcon} from "./assets/icons"
 
 function App() {
 
   return (
     <>
-      <Button size="lg" variant="primary" icon={<PlusIcon size={"lg"} />} text="Enter"  />
-      <Button size="sm" variant="secondary" icon={<PlusIcon size={"md"} />} text="Exit"  />
+      {/* <Button size="lg" variant="primary" icon={<PlusIcon size={"lg"} />} text="Enter"  />
+      <Button size="sm" variant="secondary" icon={<PlusIcon size={"md"} />} text="Exit"  /> */}
+      <div className="flex gap-4">
+        <Card title="Hi there" type="youtube" link="https://youtube.com/ALesKfHj9n4?si=landiGtOe2qae0Wt" />
+        <Card title="Hi there" type="twitter" link="https://x.com/elonmusk/status/1940029010693472616" />
+      </div>
     </>
   )
 }
--- Second_brain_app/frontend/src/assets/icons.tsx
+++ Second_brain_app/frontend/src/assets/icons.tsx
@@ -1,5 +1,4 @@
-
-interface PlusIconProps {
+interface IconProps {
     size?: "sm" | "md" | "lg"
 }
 
@@ -9,8 +8,33 @@ const sizeVariants = {
     "lg": "size-6"
 }
 
-export function PlusIcon(props: PlusIconProps) {
+export function PlusIcon(props: IconProps) {
     return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={sizeVariants[props.size || "sm"]}>
     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-  </svg>  
+    </svg>  
+}
+
+export function ShareIcon(props: IconProps){
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={sizeVariants[props.size || "md"]}>
+    <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
+    </svg>
+}
+
+export function DeleteIcon (props: IconProps) {
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={sizeVariants[props.size || "md"]}>
+    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+    </svg>
+}
+
+export function YoutubeIcon () {
+    return <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-youtube" viewBox="0 0 16 16">
+    <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/>
+    </svg>
+}
+
+export function XIcon (props: IconProps) {
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={sizeVariants[props.size || "md"]}>
+    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+    </svg>
+
 }
